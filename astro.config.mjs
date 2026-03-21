0f1b0380f76e3705ec31268547ee877ce8e83c91
dulics <ieee811@gmail.com>
2026-03-21 15:52:55 +0800

    feat: add notes, about, rss pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
 
 export default defineConfig({
   site: 'https://shannanji.com',
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
     shikiConfig: {
       theme: 'github-light',
     },
